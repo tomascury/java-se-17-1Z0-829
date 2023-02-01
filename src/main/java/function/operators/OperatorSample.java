81a299335d949ae6dad7c6d2539bf39618df45e8
Tomas Cury <tomascury@gmail.com>
2023-02-01 22:04:32 +0000

    Functional Operators

---
+++ src/main/java/function/operators/OperatorSample.java
@@ -0,0 +1,17 @@
+package function.operators;
+
+import java.util.function.*;
+
+public class OperatorSample {
+
+  public static void main(String[] args) {
+
+    // UnaryOperator<T> extends Function<T, T>
+    UnaryOperator<String> unaryOperator = name -> "My name is " + name;
+    System.out.printf("%s\n", unaryOperator.apply("Tom"));
+
+    // BinaryOperator<T> extends BiFunction<T, T, T>
+    BinaryOperator<String> binaryOperator = (s1, s2) -> s1.concat(s2);
+    System.out.printf("%s\n", binaryOperator.apply("Tom ", "Curry"));
+  }
+}
